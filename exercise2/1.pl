90be65e2cb49376db0de0ec14d4f98e1afe5f841
Ali Ariff <ali.ariff12@gmail.com>
2017-05-10 19:59:04 +0200

    solve countNumbers

--- exercise2/1.pl
+++ exercise2/1.pl
@@ -19,6 +19,21 @@ bubbleSort(L, X) :- buble(L, R), (isSorted(R) -> X = R ; bubbleSort(R, X)).
 notUnique([X, Y|Z]) :- X == Y -> true ; notUnique([Y|Z]).
 duplicates(L) :- bubbleSort(L, X), notUnique(X).
 
+increment(0, R) :- R = s(0).
+increment(s(X), R) :- R = s(s(X)).
+
+hasLast([X], X).
+hasLast([_|XS], Y) :- hasLast(XS, Y).
+
+count(_, [], 0).
+count(E, [X|L], Occ) :- E == X -> count(E, L, Occ1), increment(Occ1, Occ), !.
+count(E, [_|L], Occ) :- count(E, L, Occ).
+
+solve(C, L, []) :- hasLast(L, T), smaller(T, C), !.
+solve(C, L, [Occ|X]) :- count(C, L, Occ), increment(C, R), solve(R, L, X).
+
+countNumbers(L, X) :- bubbleSort(L, R), solve(0, R, X).
+
 :- begin_tests(exercise).
 
 test(smaller) :- smaller(0, s(0)). /* 0 < 1 */
@@ -38,12 +53,19 @@ test(smallerEqual, fail) :- smallerEqual(s(s(0)), s(0)). /* 2 <= 1 */
 test(isSorted, [nondet]) :- isSorted([0, s(0), s(0), s(s(s(0)))]). /* 0 <= 1 <= 1 <= 3 */
 test(isSorted, fail) :- isSorted([0, s(s(0)), s(0), s(s(s(0)))]). /* 0 <= 2 <= 1 <= 3 */
 
+test(buble, all(X == [[]])) :- buble([], X).
 test(buble, all(X == [[s(0), s(0), 0, s(s(0))]])) :- buble([s(0), s(s(0)), s(0), 0], X).
 test(buble, all(X == [[0, s(0), s(0), s(s(s(0)))]])) :- buble([0, s(0), s(0), s(s(s(0)))], X).
 
+test(bubbleSort, all(X == [[]])) :- bubbleSort([], X).
 test(bubbleSort, all(X == [[0, s(0), s(0), s(s(0))]])) :- bubbleSort([s(0), s(s(0)), s(0), 0], X).
 
 test(duplicates) :- duplicates([s(0), s(s(0)), s(0), 0]).
 test(duplicates, fail) :- duplicates([s(s(0)), s(0), 0]).
+
+test(countNumbers, all(X == [[s(0), s(s(0)), 0, 0, s(0)]])) :- countNumbers([s(0), s(s(s(s(0)))), s(0), 0], X).
+test(countNumbers, all(X == [[s(0), s(s(0)), 0, s(0)]])) :- countNumbers([s(0), s(s(s(0))), s(0), 0], X).
+test(countNumbers, all(X == [[s(s(s(0)))]])) :- countNumbers([0, 0, 0], X).
+test(countNumbers, all(X == [[s(s(s(0)))]])) :- countNumbers([], X).
 
 :- end_tests(exercise).
